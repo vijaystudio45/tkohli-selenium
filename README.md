37505f0f7bb15c4c92e06596a7c1f82d357e8b51
Vijay <vijaystudio45>
2022-02-09 16:55:39 +0530

    tkohli-sndright

--- README.md
+++
@@ -1,1 +0,0 @@
-# tkohli-selenium
--- filewrite.py
+++
@@ -1,5 +0,0 @@
-import pandas as pd
-import os
-path = "assets/upload_file"
-fileName = "result_admin_cases_output.xlsx"
-getFolderPath = os.path.join(path, fileName)
