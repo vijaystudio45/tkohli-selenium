504101c9950a63bcd3adc19c7cafb28557dbb941
Vijay <vijaystudio45>
2022-02-09 18:02:49 +0530

    tkohli-sndright

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def hello(k):
-	import requests
-	r = requests.post('https://dev-app.sndright.com/api/conversation_api/delete_message_merge', data ={"org_id":3,"api_key":"hello","msg_merge_id":1})
-	print(r.json())
-print(hello('join'))
+import requests
+import json
+test={"org_id":3,"api_key":"hello","msg_merge_id":1}
+r = requests.post('https://dev-app.sndright.com/api/conversation_api/delete_message_merge', data =json.dumps(test))
+print(r.json())
