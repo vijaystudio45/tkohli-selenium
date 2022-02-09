9d2f2e1c93c51fc50c00f76e3bd6a4215bda2cb9
Vijay <vijaystudio45>
2022-02-09 17:48:17 +0530

    tkohli-sndright

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
 def hello(k):
-	print('excicuted successfully')
+	import requests
+	r = requests.post('https://dev-app.sndright.com/api/conversation_api/delete_message_merge', data ={"org_id":3,"api_key":"hello","msg_merge_id":1})
+	print(r.json())
 print(hello('join'))
