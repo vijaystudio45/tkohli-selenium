7922b424037e88497033a139d6ec28f41da7a3ed
Vijay <vijaystudio45>
2022-02-10 17:30:31 +0530

    tkohli-sndright

--- hello.py
+++ hello.py
@@ -2,4 +2,5 @@ import requests
 import json
 test={"org_id":3,"api_key":"hello","msg_merge_id":1}
 r = requests.post('https://dev-app.sndright.com/api/conversation_api/delete_message_merge', data =json.dumps(test))
+
 print(r.json())
